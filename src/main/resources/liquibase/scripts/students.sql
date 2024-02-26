79c05ea081ad28c3f0c3dbd8b4a3d0daccafd97c
mezen <mezentsev.k@gmail.com>
2024-02-26 18:21:26 +0800

    done

--- src/main/resources/liquibase/scripts/students.sql
+++ src/main/resources/liquibase/scripts/students.sql
@@ -6,3 +6,6 @@ CREATE INDEX student_name_index ON student (name);
 -- changeset kmezentsev:2
 CREATE INDEX faculty_name_index ON faculty (name);
 CREATE INDEX faculty_color_index ON faculty (color);
+
+-- changeset kmezentsev:3
+CREATE INDEX faculty_name_color_index ON faculty (name, color);
